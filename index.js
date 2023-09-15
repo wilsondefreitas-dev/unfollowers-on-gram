bd79836b5a00fb5cfb978fe38bed8c4b7a9a8b08
Wilson Freitas <wilsondefreitas92@gmail.com>
2023-09-14 23:36:44 -0300

    v1

--- index.js
+++ index.js
@@ -1,52 +1,71 @@
 const puppeteer = require('puppeteer');
+const readlineSync = require('readline-sync');
 
-const DELAY = 1500;
-const USERNAME = 'wilsondef_jr';
-const PASSWORD = '$noopDogg69!';
-const TESTING = false;
+const DELAY = 2500;
+const USERNAME = readlineSync.question('Gram username: ') || '';
+const PASSWORD = readlineSync.question('Gram password: ') || '';
 
 (async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch({headless: false});
+
+  console.log('Process started, it can take some minutes...');
+
+  const browser = await puppeteer.launch({headless: 'old'});
   const page = await browser.newPage();
 
-  await login(page);  
-
-  // const followers = await getFollowers(page);
-  const followers = await getUsers(page, 'followers');
-  console.log('FINISHED FOLLOWERS');
-
-  // const following = await getFollowing(page);
-  const following = await getUsers(page, 'following');
-  console.log('FINISHED FOLLOWING');
+  try{
   
+    console.log('Loggin in...');
+    await login(page);
+
+  }catch(e){
+
+    console.log(e.message);
+    process.exit();
+
+  }
+  
+  console.log('Getting followers...');
+  const followers = await getUsers(page, 'followers');
+  console.log(`Followers list completed! You have ${followers.length} followers!`);
+
+  console.log('Getting users following you...');
+  const following = await getUsers(page, 'following');
+  console.log(`Users following you list completed! You have ${following.length} users following you!`);
+
+  console.log('Checking who is not following you back...');
   const notFollowingBack = getNotFollowingBack(followers, following);
+  console.log(`You have ${notFollowingBack.length} users not following you back. Here is their names:`);
+  
+  printUserNotFollowingBack(notFollowingBack);
 
-  console.log(`You have ${followers.length} followers`)
-  console.log(followers);
-  console.log('--------------------------')
-  console.log(`You have ${following.length} following`)
-  console.log(following);
-  console.log('--------------------------')
-  console.log(`You have ${notFollowingBack.length} suckers not following you back`)
-  console.log(notFollowingBack);
+  console.log('Process finished.')
 
-  // await browser.close();
+  await browser.close();
+  
 })();
 
 async function login(page){
 
   await page.goto('https://www.instagram.com/');
 
-  await page.waitForSelector('form'); //login form
+  await page.waitForSelector('form');
   
   await page.type('form input[name=username]', USERNAME);
   await page.type('form input[name=password]', PASSWORD);
   await page.click('form button[type=submit]');
 
-  await page.waitForSelector('._aa56'); //pop up in
+  return new Promise(async (resolve, reject) => {
 
-  return;
+    page.waitForSelector('._ab2z')
+      .then(async () => reject(
+        {message: 'Something went wrong. Check you username and password.'}
+      ));
+
+    await page.waitForSelector('._aa56');
+
+    resolve();
+
+  });
 
 }
 
@@ -56,35 +75,39 @@ async function getUsers(page, type){
   
   await page.waitForSelector('._aano'); //pop up
 
-  await scrollTillEnd(page);
+  await showAllUsersOnPopup(page);
 
   return await getUsersNames(page);
 
 }
 
-async function scrollTillEnd(page){
+async function showAllUsersOnPopup(page){
 
-  const scrollHeight = await page.evaluate(() => document.querySelector('._aano').scrollHeight);
+  let finished = false;
 
-  await page.locator('._aano').scroll({scrollTop: scrollHeight,});
+  while(!finished){ 
+    
+    const scrollHeight = await page.evaluate(() => document.querySelector('._aano').scrollHeight);
 
-  await new Promise(r => setTimeout(r, DELAY));
+    await page.locator('._aano').scroll({scrollTop: scrollHeight,});
 
-  const finished = await page.evaluate(() => {
+    await new Promise(r => setTimeout(r, DELAY));
 
-    const el = document.querySelector('._aano');
-    console.log(el.scrollHeight - el.scrollTop - el.clientHeight < 1);
-    return (el.scrollHeight - el.scrollTop - el.clientHeight < 1);
+    finished = await page.evaluate(() => {
 
-  });
+      const el = document.querySelector('._aano');
+      console.log(el.scrollHeight - el.scrollTop - el.clientHeight < 1);
+      return (el.scrollHeight - el.scrollTop - el.clientHeight < 1);
 
-  return new Promise(resolve => (finished || TESTING) ? resolve() : scrollTillEnd(page));
+    });
+  
+  }
+
+  return; 
 
 }
 
 async function getUsersNames(page){
-
-  console.log('execute getUsersNames')
 
   return await page.evaluate(() => {
 
@@ -99,6 +122,12 @@ async function getUsersNames(page){
 
 function getNotFollowingBack(followers, following){
 
-  return followers.filter(data => !following.includes(data));
+  return following.filter(data => !followers.includes(data));
+
+}
+
+function printUserNotFollowingBack(notFollowingBack){
+
+  notFollowingBack.forEach((name)=> { console.log(name); });
 
 }
